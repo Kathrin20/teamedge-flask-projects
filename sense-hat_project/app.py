cd1c7e2cc73377c1b3596a8b277379727225e72e
Kathrin20 <KAuwarter@kippnyccp.org>
2021-03-27 11:22:04 -0400

    Updates

--- sense-hat_project/app.py
+++ sense-hat_project/app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, current_app as app
+from sense_hat import SenseHat 
+from time import sleep
+
+
+sense = SenseHat()
 
 app = Flask(__name__)
 
@@ -9,7 +14,8 @@ def index():
 @app.route('/success', methods= ['GET', 'POST'])
 def success():
     message = request.form['message']
-    
+    sense.show_message(str(message))
+    sense.show_message("Nice to see you there!", text_colour=[128,0,128] )
     return render_template("success.html", message = message)
 
 if __name__ == '__main__':
